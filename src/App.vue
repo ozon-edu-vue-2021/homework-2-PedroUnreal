71b46ecd8add6443ddee2e962fa48370df6e743e
PedroUnreal <pedrounreal@gmail.com>
2021-10-27 18:46:07 +0300

    tree without file inlines

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,46 @@
 <template>
   <div id="app">
     <MakeLeaf 
-    :name="this.json.name" 
-    :contents="this.json.contents"
-    :depth="0"
-    :type="this.json.type">
+      :name="this.json.name" 
+      :contents="this.json.contents"
+      :depth="0"
+      :type="this.json.type">
     </MakeLeaf>
-    
   </div>
 </template>
 
 <script>
-import MakeLeaf from "@/components/MakeLeaf.vue";
-import tree from '../public/static/node_modules'
+  import MakeLeaf from "@/components/MakeLeaf.vue";
+  import tree from '../public/static/node_modules';
 
-export default {
-  name: "App",
-  components: {
-    MakeLeaf,
-  },
-  data() {
-    return {
-      json: tree
-      
-
-    };
-  },
-};
+  export default {
+    name: "App",
+    components: {
+      MakeLeaf,
+    },
+    data() {
+      return {
+        json: tree
+      };
+    },
+  };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-body {
-  font-family: "Open Sans", sans-serif;
-  font-size: 18px;
-  font-weight: 300;
-  line-height: 1em;
-}
+  #app {
 
-.container {
-  width: 300px;
-  margin: 0 auto;
-}
+    
+  }
 
+  body {
+    font-family: "Open Sans", sans-serif;
+    font-size: 18px;
+    font-weight: 300;
+    line-height: 1em;
+  }
 
+  .container {
+    width: 300px;
+    margin: 0 auto;
+  }
 </style>
--- src/components/MakeLeaf.vue
+++ src/components/MakeLeaf.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="tree-menu">
-    <div :style="indent" v-on:click="toggleChildren" v-bind:class="[isDir ? activeClass : '']">
-      {{ name }}
+    <div :style="indent" v-on:click="toggleChildren" :class="type">
+      <span v-html="emoji[type]"> </span> 
+      <span> {{ name }} </span>
+
+      <div v-if="showChildren">
+        <MakeLeaf
+          v-for="content in contents"
+          :contents="content.contents"
+          :name="content.name"
+          :key="content.name"
+          :depth="depth + 1"
+          :type="content.type"
+        >
+        </MakeLeaf>
+      </div>
     </div>
-    <MakeLeaf
-      v-if="showChildren"
-      v-for="content in contents"
-      :contents="content.contents"
-      :name="content.name"
-      :key="content.name"
-      :depth="depth + 1"
-      :type="content.type"
-    >
-    </MakeLeaf>
   </div>
 </template>
 <script>
@@ -21,16 +24,45 @@ export default {
   name: "MakeLeaf",
   computed: {
     indent() {
-      return { transform: `translate(${this.depth * 50}px)` };
+      return { transform: `translate(${this.depth * 10}px)` };
     },
-    },
+  },
   data() {
-    return { showChildren: false, isDir: this.type === "directory" }    
+    return { 
+      showChildren: false,
+      emoji: {
+        directory: "&#128193;",
+        file: "&#128196;",
+        link: "	&#128279;"
+      } 
+      
+    }    
   },
   methods: {
-    toggleChildren() {
+    toggleChildren(e) {
+      e.stopPropagation();
       this.showChildren = !this.showChildren;
     },
   },
 };
 </script>
+
+ <style scoped>
+
+ .tree-menu {
+   margin: 5px  0
+ }
+
+ .directory {
+   color:grey
+ }
+
+ .file {
+   color:black
+ }
+
+ .link {
+   color:blue
+ }
+ 
+ </style>
